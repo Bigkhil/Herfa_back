14060d913e64cb69a61caf35722b6fc1800f226a
Bigkhil <khm40005000@outlook.com>
2025-01-20 13:42:41 +0200

    edited the resetPassword route method to be patch

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Auth stuff
 router.route('/login').post(authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
-router.get('/resetPassword/:token', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 // Profile Stuff
 // Protecting Routes
